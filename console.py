abfe29ee3e2fa65a19be66fd8d465495b9258e51
Anthony <ndegwaanthony300@gmail.com>
2024-02-20 23:17:28 +0300

    test_params_create

--- console.py
+++ console.py
@@ -73,7 +73,7 @@ class HBNBCommand(cmd.Cmd):
                 pline = pline[2].strip()  # pline is now str
                 if pline:
                     # check for *args or **kwargs
-                    if pline[0] is '{' and pline[-1] is'}'\
+                    if pline[0] == '{' and pline[-1] == '}'\
                             and type(eval(pline)) is dict:
                         _args = pline
                     else:
@@ -113,7 +113,7 @@ class HBNBCommand(cmd.Cmd):
         """ Overrides the emptyline method of CMD """
         pass
 
-    def do_create(self, args):
+    '''def do_create(self, args):
         """ Create an object of any class"""
         if not args:
             print("** class name missing **")
@@ -124,7 +124,52 @@ class HBNBCommand(cmd.Cmd):
         new_instance = HBNBCommand.classes[args]()
         storage.save()
         print(new_instance.id)
+        storage.save()'''
+    def do_create(self, arg):
+        if not arg:
+            print("** class name missing **")
+            return
+
+        args = arg.split()
+        class_name = args[0]
+
+        # Check if the class exists
+        if class_name not in HBNBCommand.classes:
+            print("** class doesn't exist **")
+            return
+
+        # Parse parameters
+        params = {}
+        for param in args[1:]:
+            # Split parameter into key and value
+            key, value = param.split('=')
+            # Handle value based on its type
+            if value.startswith('"') and value.endswith('"'):
+                # String: Remove quotes and replace underscores with spaces
+                value = value[1:-1].replace('_', ' ')
+            elif '.' in value:
+                try:
+                    # Float: Convert to float
+                    value = float(value)
+                except ValueError:
+                    # If conversion fails, skip parameter
+                    continue
+            else:
+                try:
+                    # Integer: Convert to int
+                    value = int(value)
+                except ValueError:
+                    # If conversion fails, skip parameter
+                    continue
+
+            # Add parameter to dictionary
+            params[key] = value
+        
+        print("Params:", params)
+        # Create object with given parameters
+        new_instance = HBNBCommand.classes[class_name](**params)
         storage.save()
+        print(new_instance.id)
 
     def help_create(self):
         """ Help information for the create method """
@@ -187,7 +232,7 @@ class HBNBCommand(cmd.Cmd):
         key = c_name + "." + c_id
 
         try:
-            del(storage.all()[key])
+            del (storage.all()[key])
             storage.save()
         except KeyError:
             print("** no instance found **")
@@ -272,7 +317,7 @@ class HBNBCommand(cmd.Cmd):
                 args.append(v)
         else:  # isolate args
             args = args[2]
-            if args and args[0] is '\"':  # check for quoted arg
+            if args and args[0] == '\"':  # check for quoted arg
                 second_quote = args.find('\"', 1)
                 att_name = args[1:second_quote]
                 args = args[second_quote + 1:]
@@ -280,10 +325,10 @@ class HBNBCommand(cmd.Cmd):
             args = args.partition(' ')
 
             # if att_name was not quoted arg
-            if not att_name and args[0] is not ' ':
+            if not att_name and args[0] != ' ':
                 att_name = args[0]
             # check for quoted val arg
-            if args[2] and args[2][0] is '\"':
+            if args[2] and args[2][0] == '\"':
                 att_val = args[2][1:args[2].find('\"', 1)]
 
             # if att_val was not quoted arg
@@ -319,6 +364,7 @@ class HBNBCommand(cmd.Cmd):
         """ Help information for the update class """
         print("Updates an object with new information")
         print("Usage: update <className> <id> <attName> <attVal>\n")
+
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
--- models/base_model.py
+++ models/base_model.py
@@ -6,7 +6,7 @@ from datetime import datetime
 
 class BaseModel:
     """A base class for all hbnb models"""
-    def __init__(self, *args, **kwargs):
+    '''def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         if not kwargs:
             from models import storage
@@ -20,6 +20,26 @@ class BaseModel:
             kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
             del kwargs['__class__']
+            self.__dict__.update(kwargs)'''
+    def __init__(self, *args, **kwargs):
+        """Instantiates a new model"""
+        if not kwargs:
+            from models import storage
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            storage.new(self)
+        else:
+            if 'created_at' not in kwargs:
+                kwargs['created_at'] = datetime.now()
+            if 'updated_at' not in kwargs:
+                kwargs['updated_at'] = datetime.now()
+            if isinstance(kwargs['created_at'], str):
+                kwargs['created_at'] = datetime.strptime(kwargs['created_at'], '%Y-%m-%dT%H:%M:%S.%f')
+            if isinstance(kwargs['updated_at'], str):
+                kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'], '%Y-%m-%dT%H:%M:%S.%f')
+            if '__class__' in kwargs:
+                del kwargs['__class__']
             self.__dict__.update(kwargs)
 
     def __str__(self):
