de13b82715a5673b21600adaf8b258c817b7e1cb
Anthony <ndegwaanthony300@gmail.com>
2024-02-20 23:35:53 +0300

    setup_mysql_dev.sql

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,13 @@
+-- This script prepares a MySQL server for the AirBnB project
+
+-- Creating the database hbnb_dev_db
+CREATE DATABASE IF NOT EXIST hbnb_dev_db;
+
+-- creating a user
+CREATE USER IF NOT EXIST 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+
+-- Granting the priviledge on hbnb_dev_db
+GRANT ALL PRIVILEDGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+
+-- Give select priviledge on performance_schema to hbnb_dev_db
+GRANT  SELECT PRIVILEDGES ON performance_schema.* TO 'hbnb_dev_db'@'localhost';
