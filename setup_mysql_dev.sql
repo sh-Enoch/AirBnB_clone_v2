079923d2d60a7b165cdf49f8bbb4d896ada78828
sh-Enoch <wekesawafula.enoch@gmail.com>
2024-02-22 07:30:16 +0000

    setup test db

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,16 +1,11 @@
--- This script prepares a MySQL server for the AirBnB project
-
--- Setting the password policy to LOW
-SET GLOBAL validate_password_policy = LOW;
-
--- Creating the database hbnb_dev_db
+-- Prepares a MYSQL server for project
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-
--- creating a user
+-- Create a new user
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 
--- Granting the priviledge on hbnb_dev_db
-GRANT ALL PRIVILEDGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT  ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
+-- Grant SELECT on performance schema.
 
--- Give select priviledge on performance_schema to hbnb_dev_db
-GRANT  SELECT PRIVILEDGES ON performance_schema.* TO 'hbnb_dev_db'@'localhost';
+GRANT SELECT  ON performance_schema.* TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
