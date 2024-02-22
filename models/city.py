be35ecb07e6133afe2e097be481a713fa3705571
ndegwaanth <101396314+ndegwaanth@users.noreply.github.com>
2024-02-23 02:36:28 +0300

    Update city.py

--- models/city.py
+++ models/city.py
@@ -1,10 +1,15 @@
+#!/usr/bin/python3
+
 from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 from models.base_model import BaseModel, Base
 
+
 class City(BaseModel, Base):
-    """The city class, contains state ID and name"""
+    """ The city class, contains state ID and name """
 
     __tablename__ = 'cities'
 
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    places = relationship("Place", cascade="all, delete", backref="cities")
