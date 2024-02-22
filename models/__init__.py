a437d3e2410f9e5ffa8dde9117187be45632969f
ndegwaanth <101396314+ndegwaanth@users.noreply.github.com>
2024-02-23 02:39:16 +0300

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,11 @@
-#!usr/bin/python3
+#!/usr/bin/python3
+"""This module initializes the storage mechanism based on environment variable"""
 
 import os
+from models.state import State
 
+
+# Import necessary storage classes
 if os.getenv('HBNB_TYPE_STORAGE') == 'db':
     from models.engine.db_storage import DBStorage
     storage = DBStorage()
@@ -9,4 +13,5 @@ else:
     from models.engine.file_storage import FileStorage
     storage = FileStorage()
 
+# Reload storage after instantiation
 storage.reload()
