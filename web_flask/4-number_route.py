ee44a6ee80810d26f031938f8ff527066a9fc37c
Anthony <ndegwaanthony300@gmail.com>
2024-03-24 23:09:40 +0300

    4-number_route.py

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,34 +1,30 @@
 #!/usr/bin/python3
 
-from flask import Flask
 
+from flask import Flask
 
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
-def hello():
-    """Route that displays "Hello HBNB!"."""
-    return 'Hello HBNB!'
+@app.route("/", strict_slashes=False)
+def index():
+    return "Hello HBNB"
 
 
-@app.route('/hbnb', strict_slashes=False)
-def hbnb():
-    """Route that displays "HBNB"."""
-    return 'HBNB'
+@app.route("/hbnb", strict_slashes=False)
+def show():
+    return "HBNB"
 
 
-@app.route('/c/<text>', strict_slashes=False)
-def c_text(text='is_cool'):
-    """Route that displays "C ", followed by the value of the text variable."""
-    return 'C ' + text.replace('_', ' ')
+@app.route("/python/<string: name>", strict_slashes=False)
+def python(name="is_cool"):
+    return "Python %s" % (name).replace("_", " ")
 
 
-@app.route('/python/<text>', strict_slashes=False)
-def python_text(text='is_cool'):
-    """Route displays "Python ", followed by the value of the text variable"""
-    return 'Python ' + text.replace('_', ' ')
+@app.route("/number/<n: integer>", strict_slashes=False)
+def number(n):
+    return "%d is a number" % (n)
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(debug=True, host=0.0.0.0.0, port=5000)
