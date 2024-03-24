a7c5c5a2403104a0d8ca4a2c60fa94a6befdf4a7
Anthony <ndegwaanthony300@gmail.com>
2024-03-24 21:28:26 +0300

    2-c_route.py

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""more of the path of the url and the listening to """
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def index():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def show():
+    return "HBNB"
+
+
+@app.route("/c/string: name", strict_slashes=False)
+def display(name):
+    return "C %s" % (name)
+
+
+if __name__ == "__main__":
+    app.run(debug=True,host='0.0.0.0', port=5000)
