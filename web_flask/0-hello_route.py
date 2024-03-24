4702fc0568be6ee9f976e3ec8bb5b06e61664092
Anthony <ndegwaanthony300@gmail.com>
2024-03-24 20:48:52 +0300

    0-hello_route.py

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Script tot start flask web"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    """This method display a string"""
+    return "Hello HBNB"
+
+@app.route("/hbnb")
+def display():
+    """This metthod only display HBNB"""
+    return "HBNB"
+
+if __name__ == "__main__":
+    app.run(debug=True)
