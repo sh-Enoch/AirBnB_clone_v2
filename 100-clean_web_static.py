f1e0a23a7c4f69c9bd87c92a465a0ea02c2833af
sh-Enoch <wekesawafula.enoch@gmail.com>
2024-03-27 04:43:02 +0000

    delete outdated archive

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,14 +1,9 @@
 #!/usr/bin/python3
-"""
-Deletes out-of-date archives
-fab -f 100-clean_web_static.py do_clean:number=2
-    -i ssh-key -u ubuntu > /dev/null 2>&1
-"""
-
+# Fabfile to delete out-of-date archives.
 import os
 from fabric.api import *
 
-env.hosts = ['52.87.155.66', '54.89.109.87']
+env.hosts = ["100.25.132.152", "3.80.18.107"]
 
 
 def do_clean(number=0):
