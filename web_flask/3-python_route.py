fd76bb7140f3e07b9e92b2b03edd61a917a574ad
sh-Enoch <wekesawafula.enoch@gmail.com>
2024-05-18 21:42:09 +0000

    dynamic routing

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""more of the path of the url and the listening to """
+"""More of the path of the url and the listening to."""
 
 from flask import Flask
 
@@ -8,24 +8,30 @@ app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def index():
+    """Hello."""
     return "Hello HBNB!"
 
 
 @app.route("/hbnb", strict_slashes=False)
 def show():
+    """Hbnb."""
     return "HBNB"
 
 
 @app.route("/c/<text>", strict_slashes=False)
 def display(text):
+    """Display."""
     text = text.replace('_', ' ')
     return (f'C {text}')
+
 
 @app.route("/python/", defaults={'text': "is cool"}, strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
 def python(text):
+    """Dynamic route."""
     text = text.replace("_", " ")
     return (f"Python {text}")
+
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=5000)
